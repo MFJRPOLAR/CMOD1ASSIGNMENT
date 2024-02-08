1f03845a7963430ee9ddcb66c7cbec47ff7a12fa
Christopher Velasco <velascochristopher96@gmail.com>
2024-02-08 10:47:44 -0500

    commit

--- App.c
+++ App.c
@@ -1,5 +1,15 @@
 // pre-processor directives
 
 // include needed C and header files
+#include "Assignment.c"
 
 // write code for main function
+int main(void){
+    printf("Intensiy \t Target Heart Beat\n");
+    heartRate(age,restingPulse);
+
+    printf("Intensiy \t Target Heart Beat\n");
+    heartRateReference(&age,&restingPulse);
+
+}
+
--- Assignment.c
+++ Assignment.c
@@ -1,5 +1,32 @@
 // pre-processor directives
 
 // include needed header file
+#include "./headers./Assignment.h"
 
 // write code for functions declared in header file
+void heartRate (int, int){
+    float targetHeartRate;
+    float intensity = START;
+    do{
+        targetHeartRate = ((220 - age)- restingPulse) * (intensity) + restingPulse;
+        printf(" %.0f   \t           %.0f\n",intensity*100,targetHeartRate);
+        
+        intensity += .05;
+    }while(STOP(intensity));
+
+}
+
+void heartRateReference (int *age, int*restingPulse){
+    float targetHeartRate;
+    float intensity = START;
+    
+    do{
+        targetHeartRate = ((220 - *age)- *restingPulse) * (intensity) + *restingPulse;
+        printf(" %.0f   \t           %.0f\n",intensity*100,targetHeartRate);
+        
+        intensity += .05;
+    }while(STOP(intensity));
+
+}
+   
+   
